c34721566c8a029a45e774258bbdfa3a6b386d75
sarthak94 <sarthak@haribhakti.com>
2014-12-06 12:59:55 -0500

    Added two buttons and texting works

--- app/src/main/java/com/jacobniebloom/ourapplication/MainActivity.java
+++ app/src/main/java/com/jacobniebloom/ourapplication/MainActivity.java
@@ -1,6 +1,9 @@
 package com.jacobniebloom.ourapplication;
 
 import android.app.Activity;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.ActionBar;
 import android.support.v4.app.Fragment;
@@ -8,6 +11,8 @@ import android.support.v4.app.FragmentManager;
 import android.content.Context;
 import android.os.Build;
 import android.os.Bundle;
+import android.telephony.SmsManager;
+import android.util.Log;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.Menu;
@@ -16,12 +21,12 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.support.v4.widget.DrawerLayout;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.TextView;
-
+import android.widget.Toast;
 
 public class MainActivity extends ActionBarActivity
 		implements NavigationDrawerFragment.NavigationDrawerCallbacks {
-
 	/**
 	 * Fragment managing the behaviors, interactions and presentation of the navigation drawer.
 	 */
@@ -31,6 +36,9 @@ public class MainActivity extends ActionBarActivity
 	 * Used to store the last screen title. For use in {@link #restoreActionBar()}.
 	 */
 	private CharSequence mTitle;
+
+    private Button btn_ring;
+    private Button shareLocation;
 
 	@Override
 	protected void onCreate (Bundle savedInstanceState) {
@@ -45,7 +53,35 @@ public class MainActivity extends ActionBarActivity
 		mNavigationDrawerFragment.setUp(
 				R.id.navigation_drawer,
 				(DrawerLayout) findViewById(R.id.drawer_layout));
+
+        //set up dah button
+        btn_ring = (Button) findViewById(R.id.ringButton);
+        shareLocation  = (Button) findViewById(R.id.shareLocation);
+        btn_ring.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.v("OurApp","Button Clicked");
+                sendSMSManager();
+            }
+        });
+
 	}
+
+    public  void sendSMSManager() {
+        Toast.makeText(getApplicationContext(), "Starting text", Toast.LENGTH_LONG).show();
+        try {
+            String smsNumber = "9733095897";
+            String smsText = "Poop";
+
+            SmsManager.getDefault().sendTextMessage("9733095897", null, "Jacob, I am outside", null, null);
+            SmsManager.getDefault().sendTextMessage("3124205033", null, "Sar, I am outside", null, null);
+
+            Toast.makeText(getApplicationContext(), "Sent", Toast.LENGTH_LONG).show();
+        } catch (Exception ex) {
+            Toast.makeText(getApplicationContext(), "Crash", Toast.LENGTH_LONG).show();
+            ex.printStackTrace();
+        }
+    }
 
 	@Override
 	public void onNavigationDrawerItemSelected (int position) {
